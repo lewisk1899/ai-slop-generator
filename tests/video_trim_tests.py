e6930ae34181e450da565628e6483a55e1d7804f
Adam Mekhail <aomekhail@gmail.com>
2025-10-26 19:34:13 -0700

    moved trimmer to a new file

--- tests/video_trim_tests.py
+++ tests/video_trim_tests.py
@@ -1,7 +1,32 @@
 import unittest
+import ffmpeg
+from trimmer import trim
 
 
 class TrimTest(unittest.TestCase):
 
+    # Tests:
+    # 1. Gets the video clip length
+    # 2. Gets the amount of clips that were specified
+    # 3. Ensures the naming of the clips are done correctly
+
     def test_get_video_length(self):
+
+        file_path = "../downloads/Nek Minute - Original.mp4"
+        VIDEO_LENGTH = 47.438356
+
+        probe_result = ffmpeg.probe(file_path)
+        duration = probe_result.get("format", {}).get("duration", None)
+
+        self.assertAlmostEquals(video_length, duration)
+
+    def test_get_num_clips(self):
+        file_path = "../downloads/Nek Minute - Original.mp4"
+        out_path = "../clips"
+        time_stamps = [(4, 10), (15, 18)]
+        trim(file_path, out_path, time_stamps)
         pass
+
+
+if __name__ == "__main__":
+    unittest.main()
--- video_trim.py
+++ video_trim.py
@@ -1,7 +1,6 @@
 import argparse
-import os
 import ast
-import ffmpeg
+from trimmer import trim
 
 
 def main():
@@ -37,56 +36,6 @@ def main():
         print("Clips made!")
     else:
         print("something went wrong")
-
-
-def trim(video_clip: str, out: str, times: list) -> bool:
-    """
-    Trims a video into one or more clips depending on the amount of time ranges passed in.
-
-    Parameters
-    ----------
-
-    video_clip : str
-        the file path for the video clip
-    out : str
-        the file output path for the clip(s)
-    times : list
-        list of time ranges to create clips from
-
-    Returns
-    -------
-    bool
-        if the clip(s) were made sucessfully
-    """
-    if os.path.exists(video_clip) == False:
-        print(f"Video: {video_clip} no found")
-        return False
-
-    os.makedirs(out, exist_ok=True)
-
-    probe_result = ffmpeg.probe(video_clip)
-    duration = probe_result.get("format", {}).get("duration", None)
-    print(duration)
-
-    input_stream = ffmpeg.input(video_clip)
-
-    pts = "PTS-STARTPTS"
-
-    base_name = os.path.splitext(os.path.basename(video_clip))[0]
-
-    for idx, (start, end) in enumerate(times, start=1):
-        video = input_stream.trim(start=start, end=end).setpts(pts)
-        audio = input_stream.filter("atrim", start=start, end=end).filter(
-            "asetpts", pts
-        )
-        video_and_audio = ffmpeg.concat(video, audio, v=1, a=1)
-
-        output_path = os.path.join(out, f"{base_name}_clip_{idx}.mp4")
-        ffmpeg.output(video_and_audio, output_path, format="mp4").run(
-            overwrite_output=True
-        )
-
-    return True
 
 
 if __name__ == "__main__":
