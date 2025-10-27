fa212facb8cf0fa70986b5f7c40ac98d03733dc3
Adam Mekhail <aomekhail@gmail.com>
2025-10-26 20:27:39 -0700

    Created test cases
    Test case 3 fails. Will need to work on a fix

---
+++ tests/test_trim.py
@@ -0,0 +1,71 @@
+import unittest
+import ffmpeg
+import os
+from src.trimmer import trim
+
+
+class TrimTest(unittest.TestCase):
+
+    # Tests:
+    # 1. Gets the video clip length
+    # 2. Gets the amount of clips that were specified
+    # 3. Ensures the naming of the clips are done correctly
+
+    def test_get_video_length(self):
+
+        file_path = "downloads/Nek Minute - Original.mp4"
+        VIDEO_LENGTH = 47.438356
+
+        probe_result = ffmpeg.probe(file_path)
+        duration = probe_result.get("format", {}).get("duration", None)
+
+        self.assertAlmostEqual(VIDEO_LENGTH, float(duration))
+
+    def test_get_num_clips(self):
+        file_path = "downloads/Nek Minute - Original.mp4"
+        out_path = "clips"
+        time_stamps = [(4, 10), (15, 18)]
+
+        current_dir = os.getcwd() + "/clips"
+
+        # clear the directory
+        for filename in os.listdir(current_dir):
+            file_path = os.path.join(current_dir, filename)
+            if os.path.isfile(current_dir):
+                os.remove(current_dir)
+
+        trim(file_path, out_path, time_stamps)
+
+        num_clips = len(
+            [
+                entry
+                for entry in os.listdir(current_dir)
+                if os.path.isfile(os.path.join(current_dir, entry))
+            ]
+        )
+
+        self.assertEqual(num_clips, 2)
+
+    def test_clip_names(self):
+        file_path = "downloads/Nek Minute - Original.mp4"
+        out_path = "clips"
+        time_stamps = [(4, 10)]
+        expected_name = "Nek Minute - Original_clip_1.mp4"
+
+        current_dir = os.getcwd() + "/clips"
+
+        # clear the directory
+        for filename in os.listdir(current_dir):
+            file_path = os.path.join(current_dir, filename)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+
+        trim(file_path, out_path, time_stamps)
+
+        clips = [f for f in os.listdir(current_dir) if f.endswith(".mp4")]
+
+        self.assertEqual(len(clips), 1)
+
+        clip_name = clips[0]
+
+        self.assertEqual(clip_name, expected_name)
